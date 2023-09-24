277bca60160a667b590ae075eb03c3fe3203fb95
Abrar <hossainabrar@aol.com>
2023-09-24 00:27:14 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(stringNames, eventNames){
+    const messages = [];
+    for (let i=0; i<stringNames.length; i++){
+        //debugger;
+        messages[i] = `Thank you, ${stringNames[i]}, for the wonderful ${eventNames} gift!`
+        //debugger;
+    }
+    return messages;
+}
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(integer){
+    if (integer>=0){
+        while(integer>=0){
+            console.log(integer--)
+        }
+    }   
+};
